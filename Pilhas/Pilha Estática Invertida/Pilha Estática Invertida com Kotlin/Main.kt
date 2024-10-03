db56ae95d8332d526f30e31142587c5e51a63747
caiocotrim <caio.cotrimps4@gmail.com>
2024-10-03 19:53:14 -0300

    Pilha Estática Invertida em Kotlin

---
+++ Pilhas/Pilha Estática Invertida/Pilha Estática Invertida com Kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun Main(args: Array<String>) {
+    var pilha: Empilhavel = Empilhando()
+
+    pilha.empilhar("Caio")
+    pilha.empilhar("Cotrim")
+    pilha.empilhar("Pereira")
+    pilha.imprimir()
+}
