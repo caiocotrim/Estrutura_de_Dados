ac475589b3366aef21f2bb48ee763ca5d34c34b1
caiocotrim <caiocotrimp@gmail.com>
2024-12-03 16:00:50 -0300

    Add Heap Dinamica Min

---
+++ Unidade II/Heap Dinâmica/Min/AmontoavelMin.kt
@@ -0,0 +1,14 @@
+package Min
+
+interface AmontoavelMin {
+
+    fun inserir(dado: Long)
+    fun obter(): Long?
+    fun atualizar(novoDado: Long)
+    fun extrair(): Long?
+
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+
+}
