6ee9c3710d476671097aa3546b6957561e428c68
caiocotrim <caiocotrimp@gmail.com>
2024-12-04 17:33:10 -0300

    Add Fila Dinâmica

---
+++ Unidade II/Fila Dinâmica/Enfileiravel.kr.kt
@@ -0,0 +1,12 @@
+interface Enfileiravel{
+    // Metodos Principais
+    fun enfileirar(dado: Any?) // C
+    fun atualizar(dado: Any?)  // U
+    fun frente(): Any?         // R
+    fun desenfileirar(): Any?  // D
+
+    // Metodos Auxiliares
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+}
---
+++ Unidade II/Fila Dinâmica/No2.kt
@@ -0,0 +1,5 @@
+data class No2(var dado: Any? = null) {
+
+    var anterior: No2? = null
+    var proximo: No2? = null
+}
